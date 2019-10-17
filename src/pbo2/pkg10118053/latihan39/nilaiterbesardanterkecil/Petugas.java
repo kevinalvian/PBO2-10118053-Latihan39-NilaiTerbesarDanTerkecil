2348849879a746e17d2a9770193041b39b112b4a
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-10-17 21:45:19 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118053/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Alvian
+ * KELAS    : IF-2
+ * NIM      : 10118053
+ * Deskripsi Program : Membuat program nilai terbesar dan terkecil dengan 
+ * berbasis objek
+ */
+public class Petugas {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
